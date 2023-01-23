46cdc123b16cc7ed980f498aed0f3ebd805bacc4
Mikkobelly <mikiakuta0529@gmail.com>
2023-01-23 02:41:07 +0000

    Added all the functionality in script.js and updated styling

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//Add a new item to the list of items
+const newItem = () => {
+    let inputValue = $('#input').val();
+    let li = $(`<li>${inputValue}</li>`);
+    // li.append(document.createTextNode(inputValue));
+
+    if (inputValue === '') {
+        alert('You must write something!');
+    } else {
+        let list = $('#list');
+        list.append(li);
+    }
+
+    //Crossing out item from the list of items
+    function crossOut() {
+        li.toggleClass('strike');
+    };
+
+    li.on('dblclick', crossOut);
+
+    //Add the delete button "X"
+    let crossButton = $('<button>X</button>');
+    crossButton.addClass('btn-delete');
+    li.append(crossButton);
+
+    crossButton.on('click', deleteListItem);
+
+    //Delete item from the list (Add class 'delete')
+    function deleteListItem() {
+        li.addClass('delete');
+    };
+
+    //Reordering the items
+    $('#list').sortable();
+};
